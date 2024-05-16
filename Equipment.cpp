a0ee4d9558ebe60fefca0a09a6df44bd39f899f9
Jenil Modi <jenilmodi2612@gmail.com>
2024-05-16 14:02:50 +0930

    Update Equipment.cpp

--- Equipment.cpp
+++ Equipment.cpp
@@ -1,21 +1,23 @@
 #include "Equipment.h"
 #include <iostream>
 
+// Constructor to initialize equipment attributes
 Equipment::Equipment(std::string equipType, std::string equipCondition, std::string availStatus) :
     type(equipType), condition(equipCondition), availabilityStatus(availStatus) {}
 
+// Function to update the condition of the equipment
 void Equipment::updateCondition(std::string newCondition) {
     condition = newCondition;
 }
 
+// Function to check the availability status of the equipment
 std::string Equipment::checkAvailability() const {
     return availabilityStatus;
 }
 
+// Function to display all details of the equipment
 void Equipment::displayDetails() const {
     std::cout << "Type: " << type << std::endl;
     std::cout << "Condition: " << condition << std::endl;
     std::cout << "Availability: " << availabilityStatus << std::endl;
 }
-
-
